f2dd601dca0983ceebbf5fb07f73b93557631812
Abhi Singh <156217394+urfatherisno1@users.noreply.github.com>
2025-11-15 13:32:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -40,8 +40,8 @@ SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/imagine_iq")
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/azad_parindaaa")
 
 
-YOUR_API_URL = "http://43.205.209.72:8000"
-YOUR_API_KEY = "abhi_super_secret_key_change_me"
+YOUR_API_URL = getenv("YOUR_API_URL", "http://127.0.0.1:8000")
+YOUR_API_KEY = getenv("YOUR_API_KEY", "AbhiAPI")
 
 # Assistant settings
 AUTO_LEAVING_ASSISTANT = getenv("AUTO_LEAVING_ASSISTANT", "True")
