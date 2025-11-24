c5ce5b3a3fc75d4a083146c1ea6fe5fc21f0bb33
Abhi Singh <156217394+urfatherisno1@users.noreply.github.com>
2025-11-24 17:19:36 +0530

    Update whisper.py

--- AbhiXMusic/plugins/tools/whisper.py
+++ AbhiXMusic/plugins/tools/whisper.py
@@ -111,8 +111,8 @@ async def in_help():
     answers = [
         InlineQueryResultArticle(
             title="💒 Whisper",
-            description=f"@RockXMusic_Robot [USERNAME | ID] [TEXT]",
-            input_message_content=InputTextMessageContent(f"**📍Usage:**\n\n@RockXMusic_Robot (Target Username or ID) (Your Message).\n\n**Example:**\n@RockXMusic_Robot @username I Wanna Phuck You"),
+            description=f"@MeetMusicBot [USERNAME | ID] [TEXT]",
+            input_message_content=InputTextMessageContent(f"**📍Usage:**\n\n@MeetMusicBot (Target Username or ID) (Your Message).\n\n**Example:**\n@MeetMusicBot @username I Wanna Phuck You"),
             thumb_url="https://te.legra.ph/file/3eec679156a393c6a1053.jpg",
             reply_markup=switch_btn
         )
